d8c8935e8f67296dcf9fb94f7f0345ab05ff2dfb
asmraihan <asmraihanctg14@gmail.com>
2023-11-29 22:14:04 +0600

    express

---
+++ hindsight-server/index.js
@@ -0,0 +1,33 @@
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.post(`/createIssue`, async (req, res) => {
+    const { title, description, status } = req.body
+    const result = await prisma.issue.create({
+        data: {
+            title,
+            description,
+            status
+        },
+    })
+    res.json(result)
+})
+
+
+
+
+
+app.get(`/test`, async (req, res) => {
+    res.json({ message: 'Hello World' })
+})
+
+const server = app.listen(5000, () =>
+    console.log(`
+🚀 Server ready at: http://localhost:5000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+)
